d4015da34958546e34c407fb040b02e8d260c0f3
orun <orun@agencyundone.com>
2022-08-12 16:11:06 -0400

    feat: create a base next config

---
+++ next.config.js
@@ -0,0 +1,15 @@
+  const nextConfig = {
+    trailingSlash: true,
+    reactStrictMode: true,
+    images: {
+        loader: 'custom',
+      },
+    eslint: {
+      // Warning: Dangerously allow production builds to successfully complete even if
+      // your project has ESLint errors.
+      // TODO: Remove me when all the ts errors are figured out.
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  module.exports = nextConfig;
