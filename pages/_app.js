c63d4edcfe51f3799fe4517b586c30c68dc94699
svvimming <benmunozj@gmail.com>
2022-10-20 14:51:01 -0400

    feat: Added page title and open graph image

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/main.scss';
+import Head from 'next/head'
 import Tina from '../.tina/components/TinaDynamicProvider.js'
 
 const App = ({ Component, pageProps }) => {
   return (
     <Tina>
+      <Head>
+        <title>Filecoin Moonlanding</title>
+      </Head>
       <Component {...pageProps} />
     </Tina>
   )
